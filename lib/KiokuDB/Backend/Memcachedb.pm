e60bdcc0c5d9957ce79afdb0f61d9c9683dde262
franck cuny <franck.cuny@rtgi.fr>
2009-03-11 15:01:31 +0100

    initial commit

---
+++ lib/KiokuDB/Backend/Memcachedb.pm
@@ -0,0 +1,33 @@
+package KiokuDB::Backend::Memcachedb;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+KiokuDB::Backend::Memcachedb -
+
+=head1 SYNOPSIS
+
+  use KiokuDB::Backend::Memcachedb;
+
+=head1 DESCRIPTION
+
+KiokuDB::Backend::Memcachedb is
+
+=head1 AUTHOR
+
+franck cuny E<lt>franck.cuny@rtgi.frE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
